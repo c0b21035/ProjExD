15457b999879d67ae72e90a31cc39314114d09cf
C0B21035 <c0b21035ad@edu.teu.ac.jp>
2022-10-04 14:50:12 +0900

    練習４まで終わった

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,11 +5,17 @@ def click_number(event):
     btn = event.widget
     num = int(btn["text"])
     tkm.showinfo(f"{num}",f"{num}のボタンが押されました")
-    
+
 root = tk.Tk()
 root.geometry("300x500")
 
-r, c = 0, 0
+entry = tk.Entry(root, width=10,
+font = (",40"),justify = "right")
+
+entry.grid(row=0, column=0,
+columnspan = 3)
+
+r, c = 1, 0
 for i, num in enumerate(range(9,-1, -1), 1):
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
     btn.bind("<1>",click_number)
