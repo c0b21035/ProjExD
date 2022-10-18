472adba6f73ed3169d7e88d112339e3fa4161e65
C0B21035 <c0b21035ad@edu.teu.ac.jp>
2022-10-18 14:41:17 +0900

    練習問題

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,16 +1,32 @@
 import tkinter as tk
 
-if __name__  == "__main__":
-    root = tk.Tk()
-    root.title("迷えるこうかとん")
+def key_down(event):
+    global key
+    key = event.keysys
     
-    canv = tk.Canvas(root, width=1500, height=900, bg = "black")
+def key_up(event):
+    global key
+    key = " "
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん") # 練習1
+
+    # 練習2
+    canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
 
-    tori = tk.PhotoImage(file="fig/3.png")
-    cx, cy = 300,400
+    # 練習3
+    tori = tk.PhotoImage(file="fig/5.png") 
+    cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag="tori")
 
-    root = tk.mainloop()
+    # 練習4
+    key = "" # 現在押されているキーを表す
+
+    root.bind("<KeyPress>", key_down) # 練習5
+    root.bind("<KeyRelease>", key_up) # 練習6
+    
+    root.mainloop()
 
 
