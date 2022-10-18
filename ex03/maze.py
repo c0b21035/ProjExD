9588348ab7adfb0a93f0fa58042ba61748232ebb
C0B21035 <c0b21035ad@edu.teu.ac.jp>
2022-10-18 14:53:37 +0900

    練習問題

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,12 +1,17 @@
 import tkinter as tk
+import maze_maker as mm # 練習8
 
+# 練習5
 def key_down(event):
     global key
-    key = event.keysys
-    
+    key = event.keysym
+
+
+# 練習6
 def key_up(event):
     global key
-    key = " "
+    key = ""
+
 
 def main_proc():
     global cx, cy
@@ -20,6 +25,7 @@ def main_proc():
         cx += 20
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -38,12 +44,17 @@ if __name__ == "__main__":
     key = "" # 現在押されているキーを表す
 
     # 練習5,6
-    root.bind("<KeyPress>", key_down) # 練習5
-    root.bind("<KeyRelease>", key_up) # 練習6
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)    
 
     # 練習7
     main_proc()
-    
+
+    # 練習9,10
+    maze_list = mm.make_maze(15,9)
+    # print(maze_list)
+    mm.show_maze(canv, maze_list)
+     
     root.mainloop()
 
 
