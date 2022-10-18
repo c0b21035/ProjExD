93c36491a74685c2a034e829822e7752b616747a
C0B21035 <c0b21035ad@edu.teu.ac.jp>
2022-10-18 18:03:26 +0900

    追加機能

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,45 +1,63 @@
 import tkinter as tk
-from tkinter.tix import MAX
-import maze_maker as mm # 練習8
+import maze_maker as mm
 import random
 
-# 練習5
 def key_down(event):
     global key
     key = event.keysym
 
-def count_up():
-    global tmr
-    tmr = tmr+1
-    label["text"] = tmr
-    root.after(1000, count_up)
-
-
-# 練習6
 def key_up(event):
     global key
     key = ""
 
+def count_up():
+    global tmr, jid
+    tmr = tmr + 1
+    label1["text"] = tmr
+    jid = root1.after(1000, count_up)
+
 def main_proc():
-    global cx, cy, mx, my
-    if key == "Up":
+    global mx, my
+    global cx, cy
+    global tori
+    if key == "w":
+        tori = tk.PhotoImage(file=f"fig/4.png")
+        canv.create_image(cx, cy, image=tori, tag="tori")
         my -= 1
-    if key == "Down":
+    if key == "s":
+        tori = tk.PhotoImage(file=f"fig/3.png")
+        canv.create_image(cx, cy, image=tori, tag="tori")
         my += 1
-    if key == "Left":
+    if key == "a":
+        tori = tk.PhotoImage(file=f"fig/1.png")
+        canv.create_image(cx, cy, image=tori, tag="tori")
         mx -= 1
-    if key == "Right":
+    if key == "d":
+        tori = tk.PhotoImage(file=f"fig/6.png")
+        canv.create_image(cx, cy, image=tori, tag="tori")
         mx += 1
     if maze_list[my][mx] == 0:
-        cx, cy =mx*100+50, my*100+50
+        cx, cy = mx*100+50, my*100+50
+        if key == "w":
+            tori = tk.PhotoImage(file=f"fig/4.png")
+            canv.create_image(cx, cy, image=tori, tag="tori")
+        if key == "s":
+            tori = tk.PhotoImage(file=f"fig/3.png")
+            canv.create_image(cx, cy, image=tori, tag="tori")
+        if key == "a":
+            tori = tk.PhotoImage(file=f"fig/1.png")
+            canv.create_image(cx, cy, image=tori, tag="tori")
+        if key == "d":
+            tori = tk.PhotoImage(file=f"fig/6.png")
+            canv.create_image(cx, cy, image=tori, tag="tori")
     else:
-        if key == "Up":
+        if key == "w":
             my += 1
-        if key == "Down":
+        if key == "s":
             my -= 1
-        if key == "Left":
+        if key == "a":
             mx += 1
-        if key == "Right":
+        if key == "d":
             mx -= 1
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
@@ -47,40 +65,40 @@ def main_proc():
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん") # 練習1
     root1 = tk.Tk()
-    label = tk.Label(root1,font=("", 80))
-    label.pack()
+    root.title("迷えるこうかとん")
+    root1.title()
+    label1 = tk.Label(root1, font=("", 80))
+    label1.pack()
+    root1.after(1000, count_up)
+    tmr = 0
+    jid = None
 
-    # 練習2
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
 
-    # 練習9,10
-    maze_list = mm.make_maze(15,9)
+    #練習9,10
+    maze_list = mm.make_maze(15, 9)
     # print(maze_list)
     mm.show_maze(canv, maze_list)
-
-    # 練習3
-    tori = tk.PhotoImage(file="fig/5.png") 
+    
+    tori = tk.PhotoImage(file=f"fig/3.png")
     mx, my = 1, 1
-    cx, cy = 300+50, 400+50
+    cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image=tori, tag="tori")
 
-    # 練習4
-    key = "" # 現在押されているキーを表す
+    key = "" #現在押されているキーを表す変数
 
-    # 練習5,6
+    #練習5,6
     root.bind("<KeyPress>", key_down)
-    root.bind("<KeyRelease>", key_up)    
+    root.bind("<KeyRelease>", key_up)
 
-    # 練習7
+    #練習7
     main_proc()
 
-    root.mainloop()
+    root.after(1000, count_up)
 
-    #root.after(1000, count_up)
-    root.bind("<KeyPress>", key_down)
+    root = tk.mainloop()
    
 
 
